97d678267ac643df63b6283978e9e2c2e59e7a2a
CircularBox <158319127+CircularBox@users.noreply.github.com>
2024-04-15 00:44:19 -0400

    Game Scripts

---
+++ Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+    public Vignette_Take_Damage vignetteEffect;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        if (currentHealth <= 0) // Check health before damage reduction
+        {
+            Die();
+            return; // Exit the method if player is already dead
+        }
+
+        Debug.Log("Damage Taken");
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            Die();
+            return;
+        }
+        else
+        {
+            StartCoroutine(vignetteEffect.TakeDamageEffect());
+        }
+    }
+
+    private void Die()
+    {
+        // Implement player death logic here (e.g., play animation, disable movement, display game over screen)
+        Debug.Log("Player Died!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
